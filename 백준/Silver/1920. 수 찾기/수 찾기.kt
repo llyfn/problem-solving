103c1b4f31c4bb9477179d9f069251f272319d02
Taeyun Eom <tyv001008@gmail.com>
2024-12-29 13:31:49 +0900

    [Silver IV] Title: 수 찾기, Time: 1656 ms, Memory: 77412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,9 +1,7 @@
 fun main() = with(System.`in`.bufferedReader()) {
     readLine()
-    val a = readLine().split(' ').map { it.toInt() }.sorted()
+    val a = readLine().split(' ').sorted()
     readLine()
-    readLine().split(' ').map { it.toInt() }.forEach {
-        println(if (a.binarySearch(it) >= 0) 1 else 0)
-    }
+    readLine().split(' ').forEach { println(if (a.binarySearch(it) >= 0) 1 else 0) }
     close()
 }
