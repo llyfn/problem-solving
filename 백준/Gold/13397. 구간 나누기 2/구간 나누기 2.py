909641b08c3e898c44ac50e6a5e586c45966aa3c
Taeyun Eom <tyv001008@gmail.com>
2024-12-29 10:17:15 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 132 ms, Memory: 111004 KB -BaekjoonHub

---
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def divide(x):
+  max_x = min_x = arr[0]
+  cnt = 1
+  for i in range(1, n):
+    max_x = max(max_x, arr[i])
+    min_x = min(min_x, arr[i])
+    if max_x - min_x > x:
+      cnt += 1
+      max_x = arr[i]
+      min_x = arr[i]
+  return cnt
+
+start, end = 0, max(arr)
+result = 0
+
+while start <= end:
+  mid = (start + end) // 2
+  if divide(mid) <= m:
+    end = mid - 1
+    result = mid
+  else:
+    start = mid + 1
+
+print(result)
