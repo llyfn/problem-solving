746ebd6e96c1ac1ea2ffb5c1f4b9b764e1e29c26
Taeyun Eom <tyv001008@gmail.com>
2024-12-29 15:51:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 110944 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,6 +1,6 @@
 k, n = map(int, input().split())
 c = [int(input()) for _ in range(k)]
-l = m = 0; r = max(c) * 2
+l = m = 1; r = max(c)
 while l <= r:
     m = (l + r) // 2
     if sum([x // m for x in c]) >= n: l = m + 1
