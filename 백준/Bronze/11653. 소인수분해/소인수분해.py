1955459bde54f6f8602df14467787bdd156f0393
Taeyun Eom <tyv001008@gmail.com>
2024-12-29 10:16:34 +0900

    [Bronze I] Title: 소인수분해, Time: 112 ms, Memory: 109416 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,45 @@
+import math, random
+
+def pollard_rho(n):
+    def f(m):
+        if m == 1: return
+        if not m & 1:
+            yield 2
+            yield from f(m >> 1)
+            return
+        if miller_rabin(m):
+            yield m
+            return
+
+        g = m
+        while True:
+            if g == m:
+                x = y = random.randint(2, m - 1)
+                c = random.randint(1, 20)
+            x = (x ** 2 + c) % m
+            y = ((y ** 2 + c) % m) ** 2 + c % m
+            g = math.gcd(abs(x - y), m)
+            if g != 1: break
+
+        yield from f(g)
+        yield from f(m // g)
+
+    return sorted(f(n))
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+def miller_rabin(n):
+    def test(m, a):
+        if m % a == 0: return False
+        d = m - 1
+        while True:
+            if (t := pow(a, d, m)) == 1 or t == m - 1: return True
+            elif d & 1: return False
+            d >>= 1
+
+    if n in primes: return True
+    elif not n & 1: return False
+    for p in primes:
+        if not test(n, p): return False
+    return n > 40
+
+print(*pollard_rho(int(input())), sep='\n')
