81a05a78634390962f804f8ba12f35910b735f4e
Taeyun Eom <tyv001008@gmail.com>
2024-12-30 22:15:39 +0900

    [Gold V] Title: 동전, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -2,10 +2,7 @@ for _ in range(int(input())):
     n = int(input())
     v = [*map(int, input().split())]
     m = int(input())
-    dp = [[1] + [0] * m] * 2
-    for c in range(n):
-        p = c % 2; q = +(not p)
-        if c > 0: dp[p] = dp[q][:]
-        for i in range(c, m + 1):
-            if i >= v[c]: dp[p][i] += dp[p][i - v[c]]
-    print(dp[not n % 2][-1])
+    d = [1] + [0] * m
+    for x in v:
+        for i in range(x, m + 1): d[i] += d[i - x]
+    print(d[-1])
