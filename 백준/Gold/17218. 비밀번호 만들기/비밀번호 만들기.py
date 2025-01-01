c7ca79fbcc4bf05471115cbc0624f823f9d087c2
Taeyun Eom <tyv001008@gmail.com>
2025-01-01 17:37:58 +0900

    [Gold IV] Title: 비밀번호 만들기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.py
@@ -1,12 +1,5 @@
-a = input(); b = input(); p = len(a); q = len(b)
-d = [[0] * (q + 1) for _ in range(p + 2)]
+a,b=input(),input();p,q=len(a),len(b)
+d=[['']*(q+1)for _ in range(p+1)]
 for i in range(p):
-    for j in range(q):
-        if a[i] == b[j]: d[i + 1][j + 1] = d[i][j] + 1
-        else: d[i + 1][j + 1] = max(d[i][j + 1], d[i + 1][j])
-c = d[p][q]; seq = ''
-while c:
-    if d[p][q] == d[p - 1][q]: p -= 1
-    elif d[p][q] == d[p][q - 1]: q -= 1
-    else: seq += a[p - 1]; c -= 1; p -= 1; q -= 1
-print(seq[::-1])
+    for j in range(q): d[i+1][j+1]=d[i][j]+a[i]if a[i]==b[j]else max(d[i+1][j],d[i][j+1],key=len)
+print(d[p][q])
