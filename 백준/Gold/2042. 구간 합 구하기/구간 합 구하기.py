dc5b00f57c5bd97f3181c94ea8a81cd3a775810d
Taeyun Eom <tyv001008@gmail.com>
2025-01-03 21:28:46 +0900

    [Gold I] Title: 구간 합 구하기, Time: 484 ms, Memory: 136548 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -11,19 +11,19 @@ for i in range(n): tree[p + i] = L[i]
 for i in range(p - 1, 0, -1): tree[i] = tree[2 * i] + tree[2 * i + 1]
 
 def update(i, v):
-    idx = p + i - 1
-    d = v - tree[idx]
-    while idx: tree[idx] += d; idx //= 2
+    i += p - 1
+    d = v - tree[i]
+    while i: tree[i] += d; i //= 2
 
 def isum(l, r):
     a = []
-    s, e = p + l - 1, p + r - 1
-    while s < e:
-        if s & 1: a += tree[s],
-        if not e & 1: a += tree[e],
-        s = (s + 1) // 2
-        e = (e - 1) // 2
-    if s == e: a += tree[s],
+    l += p - 1; r += p - 1
+    while l < r:
+        if l & 1: a += tree[l],
+        if not r & 1: a += tree[r],
+        l = (l + 1) // 2
+        r = (r - 1) // 2
+    if l == r: a += tree[l],
     return sum(a)
 
 for _ in range(m + k):
