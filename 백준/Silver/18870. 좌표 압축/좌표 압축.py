f6e976066461f05be74a19090464e3d01461e331
llyfn <tyv001008@gmail.com>
2025-01-06 18:46:05 +0900

    [Silver II] Title: 좌표 압축, Time: 976 ms, Memory: 303020 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,8 +1,5 @@
-n = int(input())
-x = map(int, input().split())
-y = {}
-z = [0] * n
-for idx, i in enumerate(x): y.setdefault(i, []).append(idx)
-for idx, i in enumerate(sorted(y.items(), key=lambda w: w[0])):
-    for j in i[1]: z[j] = idx
-print(*z)
+input()
+x = [*map(int, input().split())]
+y = sorted({*x})
+y = dict(zip(y, range(len(y))))
+print(*[y[i] for i in x])
