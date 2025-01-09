a843c9c196079c74ecf605aadac14ffaad9a49a5
llyfn <tyv001008@gmail.com>
2025-01-09 14:12:31 +0900

    [Silver I] Title: IOIOI, Time: 112 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,4 +1,8 @@
-import re
-p='(?=(I'+'OI'*int(input())+'))'
-input();s=input()
-print(len(re.findall(p,s)))
+n,m,s=int(input()),int(input()),input()
+a=c=i=0
+while i<m-1:
+    if s[i:i+3] == 'IOI':
+        c+=1;i+=2
+        if c==n:a+=1;c-=1
+    else: i += 1;c=0
+print(a)
