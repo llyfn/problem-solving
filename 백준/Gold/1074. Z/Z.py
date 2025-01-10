49c03aedef3de51d0a91ee9fec35159d80d52f6e
llyfn <tyv001008@gmail.com>
2025-01-10 18:12:54 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -1,5 +1,3 @@
-def f(n,r,c):
-    if n==0:return 0
-    m=1<<n-1;k=m*m
-    return 3*k+f(n-1,r-m,c-m) if r>=m and c>=m else 2*k+f(n-1,r-m,c) if r>=m else k+f(n-1,r,c-m) if c>=m else f(n-1,r,c)
-print(f(*map(int,input().split())))
+n,r,c=map(int,input().split())
+f=lambda x:sum((x>>i&1)*4**i for i in range(n))
+print(2*f(r)+f(c))
