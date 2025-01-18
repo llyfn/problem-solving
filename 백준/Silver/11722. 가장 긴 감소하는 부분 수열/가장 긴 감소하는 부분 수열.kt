f5a00294af206bddaacc8534d65fd41a66cad194
llyfn <tyv001008@gmail.com>
2025-01-18 21:13:55 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 168 ms, Memory: 26684 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.kt
@@ -0,0 +1,14 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toInt()
+    val a = readLine().split(" ").map { it.toInt() }.toIntArray()
+    val dp = IntArray(n)
+    val x = mutableListOf(Int.MAX_VALUE)
+
+    for (i in 0..<n) {
+        dp[i] = x.binarySearch(a[i], Comparator.reverseOrder()).let { if (it < 0) -it - 1 else it }
+        if (x.size <= dp[i]) x.add(a[i])
+        if (a[i] > x[dp[i]]) x[dp[i]] = a[i]
+    }
+    println(dp.max())
+    close()
+}
