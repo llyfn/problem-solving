4b1c8daf85c6e4330b6c2bdcc55b8c1ca79467e8
llyfn <tyv001008@gmail.com>
2025-01-21 18:08:59 +0900

    [Gold V] Title: 내려가기, Time: 208 ms, Memory: 121524 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,8 +1,6 @@
 I=lambda:map(int,input().split())
-n,=I();g=[[*I()]for _ in range(n)]
-M=g[0][:];m=g[0][:]
-for i in range(1,n):
-    a,b,c=g[i]
-    M=[max(M[:2])+a,max(M)+b,max(M[1:])+c]
-    m=[min(m[:2])+a,min(m)+b,min(m[1:])+c]
+n,=I()
+g=[[*I()]for _ in range(n)]
+M=m=[0]*3
+for a,b,c in g:M=[max(M[:2])+a,max(M)+b,max(M[1:])+c];m=[min(m[:2])+a,min(m)+b,min(m[1:])+c]
 print(max(M),min(m))
