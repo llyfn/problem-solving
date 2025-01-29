ea21dee647d936951f6d749b08bf64680ef37f4b
llyfn <tyv001008@gmail.com>
2025-01-29 12:03:14 +0900

    [Gold IV] Title: 행렬 제곱, Time: 100 ms, Memory: 110896 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,12 +1,9 @@
 I=lambda:map(int,input().split())
-n,k=I()
+n,e=I()
 l=[[*I()]for _ in range(n)]
 def m(a,b):return[[sum(a[i][k]*b[k][j] for k in range(n))%1000 for j in range(n)]for i in range(n)]
-def p(b,e):
-    x=[[+(i==j) for j in range(n)]for i in range(n)]
-    while e:
-        if e&1:x=m(x,b)
-        b=m(b,b);e>>=1
-    return x
-r=p(l,k)
-for i in r:print(*i)
+x=[[i==j for j in range(n)]for i in range(n)]
+while e:
+    if e&1:x=m(x,l)
+    l=m(l,l);e>>=1
+for i in x:print(*i)
