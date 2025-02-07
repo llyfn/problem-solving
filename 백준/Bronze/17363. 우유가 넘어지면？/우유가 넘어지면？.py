3e012732360f61c1eaf63e30cc468445400b4fb2
llyfn <tyv001008@gmail.com>
2025-02-07 14:27:24 +0900

    [Bronze I] Title: 우유가 넘어지면?, Time: 92 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Bronze/17363. 우유가 넘어지면？/우유가 넘어지면？.py
+++ 백준/Bronze/17363. 우유가 넘어지면？/우유가 넘어지면？.py
@@ -1,2 +1,1 @@
-n,m=map(int,input().split())
-for i in [*zip(*[input()for _ in range(n)])][::-1]:print(''.join(i).translate(str.maketrans({45:124,124:45,47:92,92:47,94:60,60:118,118:62,62:94})))
+for i in[*zip(*map(reversed,[*open(0)][1:]))][1:]:print(''.join(i).translate(str.maketrans("-|/\^<v>","|-\/<v>^")))
