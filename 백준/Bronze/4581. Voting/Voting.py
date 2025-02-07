d8344e39d30c670c55ed0d351ba12f1bc0496949
llyfn <tyv001008@gmail.com>
2025-02-07 13:42:27 +0900

    [Bronze II] Title: Voting, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/4581. Voting/Voting.py
+++ 백준/Bronze/4581. Voting/Voting.py
@@ -1,1 +1,1 @@
-while (i:=input())!='#':k=lambda x:i.count(x);l=len(i);print("need quorum"if k('A')>=l/2 else'yes'if k('Y')>k('N')else'no'if k('N')>k('Y')else'tie')
+while(i:=input())!='#':a,y,n=map(i.count,'AYN');print(["need quorum",["tie","no","yes"][(y<n)+2*(y>n)]][a<len(i)/2])
