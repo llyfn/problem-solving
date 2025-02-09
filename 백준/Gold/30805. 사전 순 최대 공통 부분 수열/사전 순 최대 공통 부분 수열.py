5169c41bc82d4801accd5e285df908efb2f3d987
llyfn <tyv001008@gmail.com>
2025-02-09 17:33:43 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
@@ -1,13 +1,4 @@
 I=lambda:[*map(int,input().split())]
-I();a=I();I();b=I()
-r=[]
-while 1:
-    while a and b:
-        i=max(a);j=max(b);k=a.index(i);l=b.index(j)
-        if i==j:break
-        elif i>j:a.pop(k)
-        else:b.pop(l)
-    else:break
-    r+=i,;a=a[k+1:];b=b[l+1:]
-print(len(r))
-if r:print(*r)
+I();a=I();I();b=I();r=[]
+while i:=set(a)&set(b):r+=(m:=max(i)),;a=a[a.index(m)+1:];b=b[b.index(m)+1:]
+print(len(r));print(*r)
