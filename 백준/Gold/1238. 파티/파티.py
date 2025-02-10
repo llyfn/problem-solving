e21479676824d05ea49059404c76683fa3eda8f2
llyfn <tyv001008@gmail.com>
2025-02-10 17:20:28 +0900

    [Gold III] Title: 파티, Time: 668 ms, Memory: 115784 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,5 +1,4 @@
 import heapq as h
-P,S=h.heappop,h.heappush
 I=lambda:map(int,input().split())
 n,m,x=I()
 g=[[] for _ in range(n+1)]
@@ -9,10 +8,10 @@ def f(s):
     d[s]=0
     q=[(0,s)]
     while q:
-        c,v=P(q)
+        c,v=h.heappop(q)
         if d[v]<c:continue
         for u,w in g[v]:
-            if d[u]>c+w:d[u]=c+w;S(q,(c+w,u))
+            if d[u]>c+w:d[u]=c+w;h.heappush(q,(c+w,u))
     return d
 r=f(x)
 for i in range(1,n+1):r[i]+=f(i)[x]
