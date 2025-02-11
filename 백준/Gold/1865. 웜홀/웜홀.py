a7a3ca04ae64946a27168e3cd460f863cb2d3fe9
llyfn <tyv001008@gmail.com>
2025-02-11 18:13:14 +0900

    [Gold III] Title: 웜홀, Time: 244 ms, Memory: 113312 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,14 +1,8 @@
 I=lambda:[*map(int,input().split())]
 for _ in[0]*I()[0]:
-    n,m,w=I();s,d=[[]for _ in range(n+1)],[1e9]*(n+1);d[1]=0
-    for _ in[0]*m:a,b,c=I();s[a]+=(b,c),;s[b]+=(a,c),
-    for _ in[0]*w:a,b,c=I();s[a]+=(b,-c),
-    for _ in[0]*n:
-        for i in range(1,n+1):
-            for j,k in s[i]:d[j]=min(d[j],d[i]+k)
-    for i in range(1,n+1):
-        for j,k in s[i]:
-            if d[j]>d[i]+k:print('YES');break
-        else:continue
-        break
+    n,m,w=I();d,s=[0]*(n+1),[I()for _ in[0]*m];s+=[[b,a,c]for a,b,c in s]+[[a,b,-c]for a,b,c in[I()for _ in[0]*w]]
+    for _ in[0]*(n-1):
+        for a,b,c in s:c+=d[a];d[b]=min(d[b],c)
+    for a,b,c in s:
+        if d[b]>(c:=c+d[a]):print('YES');break
     else:print('NO')
