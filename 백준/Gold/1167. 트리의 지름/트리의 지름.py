6f4730e4d2ae2342e99b5cad69b49d9ebc03687d
llyfn <tyv001008@gmail.com>
2025-02-15 17:55:43 +0900

    [Gold II] Title: 트리의 지름, Time: 412 ms, Memory: 165440 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,8 +1,7 @@
-import itertools as t
 I=lambda:map(int,input().split())
 n,=I()
 g=[[]for _ in[0]*(n+1)]
-for _ in[0]*n:a,*l,_=I();g[a]+=[(b,c)for b,c in t.batched(l,2)]
+for _ in[0]*n:a,*l,_=I();g[a]+=[(b,c)for b,c in zip(l[::2],l[1::2])]
 def f(s):
     v,q=[0]*(n+1),[(s,0)];d=v[:]
     while q:s,c=q.pop();v[s],d[s]=1,c;q+=[(i,c+j)for i,j in g[s]if v[i]<1]
