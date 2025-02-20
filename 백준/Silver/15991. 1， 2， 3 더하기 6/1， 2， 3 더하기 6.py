6f33c0736bba6bad2f8f4ce02dda9e1817fdf89f
llyfn <tyv001008@gmail.com>
2025-02-20 15:15:28 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 116 ms, Memory: 113664 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.py
@@ -1,4 +1,3 @@
-K=100001
-d=[1,1,2,2,3,3]+[0]*K
-for i in range(6,K):d[i]=sum(d[i-6:i:2])%(10**9+9)
-for i in[*open(0)][1:]:print(d[int(i)])
+d=[1,2,3]
+for i in[0]*9**5:d+=sum(d[-3:])%(10**9+9),
+for i in[*open(0)][1:]:print(d[int(i)//2])
