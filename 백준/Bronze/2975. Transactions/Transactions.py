a5ad247da05ff97cffc9951322a37d64495dcd88
llyfn <tyv001008@gmail.com>
2025-02-20 12:58:47 +0900

    [Bronze III] Title: Transactions, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2975. Transactions/Transactions.py
+++ 백준/Bronze/2975. Transactions/Transactions.py
@@ -1,5 +1,1 @@
-for s in open(0):
-    a,b,c=s.split()
-    if c=='0':break
-    i=int(a)+int(c)*(1-2*(b=='W'))
-    print([i,'Not allowed'][i<-200])
+for s in[*open(0)][:-1]:a,b,c=s.split();print([i:=int(a)+int(c)*(1-2*(b=='W')),'Not allowed'][i<-200])
