7474c5f07e4af6772298cacdbc41627e400a97bf
llyfn <tyv001008@gmail.com>
2025-04-29 11:21:31 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 4480 ms, Memory: 330392 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,14 +1,11 @@
 import sys
 sys.setrecursionlimit(10**6)
 N,*S=map(int,open(0).read().split())
-E=[[]for _ in range(N+1)]
-D=[[0,0]for _ in range(N+1)]
-V=[0]*(N+1)
+E=[[]for _ in[0]*(N+1)]
+D=[[1,0]for _ in[0]*(N+1)]
 for u,v in zip(S[::2],S[1::2]):E[u]+=v,;E[v]+=u,
-def f(u):
-    V[u]=D[u][0]=1
+def f(u,p):
     for v in E[u]:
-        if V[v]:continue
-        f(v);D[u][0]+=min(D[v]);D[u][1]+=D[v][0]
-f(1)
+        if v!=p:f(v,u);D[u][0]+=min(D[v]);D[u][1]+=D[v][0]
+f(1,0)
 print(min(D[1]))
