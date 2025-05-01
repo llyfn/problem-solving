348665cf461ea064deb0e36c2a611a5924195813
llyfn <tyv001008@gmail.com>
2025-05-01 14:59:47 +0900

    [Gold II] Title: 철로, Time: 420 ms, Memory: 150820 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.py
+++ 백준/Gold/13334. 철로/철로.py
@@ -1,11 +1,6 @@
 from heapq import*
 N,*P,D=map(int,open(0).read().split())
-P=sorted([sorted(P[i:i+2])for i in range(0,N*2,2)],key=lambda x:(x[1],x[0]))
+P=sorted(sorted(i)[::-1]for i in zip(P[::2],P[1::2]))
 R,Q=0,[]
-for i in P:
-    heappush(Q,i[0])
-    while(Q):
-        if i[1]-Q[0]>D:heappop(Q)
-        else:break
-    R=max(R,len(Q))
+for a,b in P:heappush(Q,b);Q and a-D>Q[0]and heappop(Q);R=max(R,len(Q))
 print(R)
