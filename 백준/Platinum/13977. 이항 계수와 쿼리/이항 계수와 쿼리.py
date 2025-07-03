f494f22ce558a3d66831c25dbf22f2aec2e1594b
Taeyun Eom <tyv001008@gmail.com>
2025-07-03 22:19:16 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 252 ms, Memory: 173924 KB -BaekjoonHub

--- 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.py
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.py
@@ -1,10 +1,12 @@
 import sys
 input = sys.stdin.readline
-m = int(input())
 M = 10 ** 9 + 7
 N = 4 * 10 ** 6
-fact = [1]
-for i in range(1, N + 1): fact.append(fact[-1] * i % M)
-for _ in range(m):
+fact = [1] * (N + 1)
+for i in range(1, N + 1): fact[i] = fact[i - 1] * i % M
+inv_fact = [1] * (N + 1)
+inv_fact[N] = pow(fact[N], M - 2, M)
+for i in range(N - 1, -1, -1): inv_fact[i] = inv_fact[i + 1] * (i + 1) % M
+for _ in range(int(input())):
     n, k = map(int, input().split())
-    print(fact[n] * pow(fact[k] * fact[n - k], M - 2, M) % M)
+    print(fact[n] * inv_fact[k] % M * inv_fact[n - k] % M)
