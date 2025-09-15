e4e15450644557984b8113995910bd2cb1357143
Taeyun Eom <tyv001008@gmail.com>
2025-09-16 03:04:27 +0900

    [Gold V] Title: 산책 나온 춘배, Time: 176 ms, Memory: 124812 KB -BaekjoonHub

--- 백준/Gold/30412. 산책 나온 춘배/산책 나온 춘배.py
+++ 백준/Gold/30412. 산책 나온 춘배/산책 나온 춘배.py
@@ -1,3 +1,4 @@
 n,x,*h=map(int,open(0).read().split())
-m=min((a:=h[k-1],b:=h[k],e:=h[k+1],min(max(0,max(a+x-b,e+x-b)),(f:=max(0,a+x-b))+max(0,b+f+x-e),(f:=max(0,e+x-b))+max(0,b+f+x-a),max(0,b+x-a)+max(0,b+x-e)))[3]if 0<k<n-1else max(0,x-abs(h[k]-h[1-3*(k>0)]))for k in range(n))
+h=[9**9,*h,9**9]
+m=min((a:=h[k],b:=h[k+1],e:=h[k+2],min(max(0,max(a+x-b,e+x-b)),(f:=max(0,a+x-b))+max(0,b+f+x-e),(f:=max(0,e+x-b))+max(0,b+f+x-a),max(0,b+x-a)+max(0,b+x-e)))[3]for k in range(n))
 print(m)
