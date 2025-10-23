baa93aa41aa3313309302a7bb1c99b31df0cec71
Taeyun Eom <tyv001008@gmail.com>
2025-10-23 23:49:07 +0900

    [Silver IV] Title: ZOAC 3, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/20436. ZOAC 3/ZOAC 3.py
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.py
@@ -1,6 +1,6 @@
 K=['zxcvbnm','asdfghjkl','qwertyuiop']
 D={c:(i,j)for i,r in enumerate(K)for j,c in enumerate(r)}
 (p,q),_,(r,s),_,*l,_=map(D.get,open(0).read())
-S=0
-for x,y in l:exec('S+=abs(%s-x)+abs(%s-y)+1;%s,%s=x,y'%(('r','s','r','s'),('p','q','p','q'))[y<4or x>0and y<5])
+S=0;F=[[r,s],[p,q]]
+for x,y in l:i=y<5and(y<4or x>0);X,Y=F[i];S+=abs(X-x)+abs(Y-y)+1;F[i]=[x,y]
 print(S)
