6de4cad62a8b9821854576c1334ec7c515a2aaa8
Taeyun Eom <tyv001008@gmail.com>
2025-10-24 16:43:10 +0900

    [Gold II] Title: 문자열 비교하기, Time: 112 ms, Memory: 111612 KB -BaekjoonHub

--- 백준/Gold/17180. 문자열 비교하기/문자열 비교하기.py
+++ 백준/Gold/17180. 문자열 비교하기/문자열 비교하기.py
@@ -1,7 +1,7 @@
 n,m=map(int,input().split())
-a,b=[[*map(ord,input())]for _ in'  ']
+a,b=input(),input()
 d=[[1e9]*-~m for i in[0]*-~n]
 d[0][0]=0
 for i in range(n):
-    for j in range(m):d[i+1][j+1]=min(d[i][j],d[i+1][j],d[i][j+1])+abs(a[i]-b[j])
+    for j in range(m):d[i+1][j+1]=min(d[i][j],d[i+1][j],d[i][j+1])+abs(ord(a[i])-ord(b[j]))
 print(d[n][m])
