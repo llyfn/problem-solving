dc4dbdad1d9bb0428162e0ee75740503a0dd9e3a
Taeyun Eom <tyv001008@gmail.com>
2025-11-07 02:26:16 +0900

    [Gold I] Title: Nivelle, Time: 428 ms, Memory: 111572 KB, Score: 110 point -BaekjoonHub

--- 백준/Gold/18329. Nivelle/Nivelle.py
+++ 백준/Gold/18329. Nivelle/Nivelle.py
@@ -1,10 +1,15 @@
-n,s=int(input()),input()
-m=1
-l=r=0
-for i in range(n):
- U=set()
- for j in range(i,n):
-  U.add(s[j])
-  c=len(U)/(j-i+1)
-  if c<m:m,l,r=c,i,j
-print(l+1,r+1)
+n=int(input());s=input()
+A=len(set(s))
+T=[(1,0,0)]
+for j in range(1,A+1):
+ C={};i=m=b=0;a=0
+ for k in range(n):
+   c=s[k];C[c]=C.get(c,0)+1
+   while len(C)>j:
+    l=s[i];C[l]-=1;i+=1
+    if C[l]==0:del C[l]
+   L=k-i+1
+   if L>m:m=L;b=i;a=len(C)
+ if m:T+=(a/m,b,b+m-1),
+m,L,R=min(T)
+print(L+1,R+1)
