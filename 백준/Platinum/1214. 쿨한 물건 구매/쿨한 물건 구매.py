7f61cedd94ac4b686901a1269bb0bc505b28435b
Taeyun Eom <tyv001008@gmail.com>
2025-11-11 15:54:15 +0900

    [Platinum V] Title: 쿨한 물건 구매, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.py
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.py
@@ -1,5 +1,5 @@
 d,p,q=map(int,input().split())
 if p>q:p,q=q,p
 m=1e11
-for i in range(min(d//q,p)+1):m=min(m,(p-(d-q*i)%p)%p)
+for i in range(min(d//q,p)+1):m=min(m,(p+q*i-d)%p)
 print(d+min(m,(q-(d%q))%q))
