09f79b2e3a6eb680caa49c094fe9e67bea20cd34
Taeyun Eom <tyv001008@gmail.com>
2026-01-12 17:43:09 +0900

    [Diamond V] Title: 특공대, Time: 416 ms, Memory: 224228 KB -BaekjoonHub

--- 백준/Diamond/4008. 특공대/특공대.py
+++ 백준/Diamond/4008. 특공대/특공대.py
@@ -4,7 +4,7 @@ s=p=0
 for w in l:
  s+=w
  while p+1<len(h)and h[p+1][0]<=s:p+=1
- if p>99:h=h[p:];p=0
+ if p>9:h=h[p:];p=0
  x,k,m=h[p]
  K=-2*a*s;M=2*a*s*s+c+k*s+m
  while len(h)>1and(h[-1][2]-M)/(K-h[-1][1])<=h[-1][0]:h.pop()
