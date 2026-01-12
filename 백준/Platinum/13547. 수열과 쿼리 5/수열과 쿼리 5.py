47953e2ce5f64c00d3bbdc74aadd3d2d3113e307
Taeyun Eom <tyv001008@gmail.com>
2026-01-12 20:23:31 +0900

    [Platinum II] Title: 수열과 쿼리 5, Time: 764 ms, Memory: 159192 KB -BaekjoonHub

--- 백준/Platinum/13547. 수열과 쿼리 5/수열과 쿼리 5.py
+++ 백준/Platinum/13547. 수열과 쿼리 5/수열과 쿼리 5.py
@@ -3,11 +3,9 @@ S=int(N**.5)
 R=[0]*M
 C=[0]*1000001
 Q=sorted([[Q[i][0]-1,Q[i][1]-1,i]for i in range(M)],key=lambda x:(x[0]//S,x[1]))
-l,r,q=Q[0]
-v=0
-for i in range(l,r+1):v+=C[A[i]]==0;C[A[i]]+=1
-R[q]=v
-for i in range(1,M):
+l=v=0
+r=-1
+for i in range(0,M):
  x,y,q=Q[i]
  while x<l:l-=1;v+=C[A[l]]==0;C[A[l]]+=1
  while x>l:C[A[l]]-=1;v-=C[A[l]]==0;l+=1
