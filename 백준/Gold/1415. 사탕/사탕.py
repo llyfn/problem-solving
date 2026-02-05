e8ce8d115333fda250dec30c1ce6f3b4247b4d12
Taeyun Eom <tyv001008@gmail.com>
2026-02-05 23:14:09 +0900

    [Gold I] Title: 사탕, Time: 516 ms, Memory: 208476 KB -BaekjoonHub

--- 백준/Gold/1415. 사탕/사탕.py
+++ 백준/Gold/1415. 사탕/사탕.py
@@ -1,22 +1,24 @@
-from collections import *
+from collections import Counter
 _, *C = map(int, open(0))
 S = sum(C)
 C = Counter(C)
-N = len(C)
-dp = [[0] * (S + 1) for _ in range(N + 1)]
-dp[0][0] = 1
-n = 0
-for c in C:
-    for s in range(S + 1):
-        dp[n + 1][s] = dp[n][s]
-        for i in range(1, C[c] + 1):
-            if i * c > s: break
-            dp[n + 1][s] += dp[n][s - i * c]
-    n += 1
-p = [1] * (S + 1)
-p[0] = 0
-if S: p[1] = 0
+Z = C.pop(0, 0)
+dp = [0] * (S + 1)
+dp[0] = 1
+for p, c in C.items():
+    T = [0] * (S + 1)
+    for r in range(p):
+        v = 0
+        for i in range(r, S + 1, p):
+            v += dp[i]
+            if i >= (c + 1) * p: v -= dp[i - (c + 1) * p]
+            T[i] = v
+    dp = T
+P = [1] * (S + 1)
+P[0] = 0
+if S: P[1] = 0
 for i in range(2, int(S ** .5) + 1):
-    if p[i]:
-        for j in range(2 * i, S + 1, i): p[j] = 0
-print(sum(dp[N][i] for i in range(S + 1) if p[i]))
+    if P[i]:
+        for j in range(i * i, S + 1, i):
+            P[j] = 0
+print(sum(dp[i] for i in range(S + 1) if P[i]) * (Z + 1))
