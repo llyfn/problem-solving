79d5db6edbd815c7a17a69d4b876dc5efceaff78
Taeyun Eom <tyv001008@gmail.com>
2026-02-19 02:29:41 +0900

    [Medium] Problem: 3100-water-bottles-ii, Time: 50 ms (21.36%), Space: 19.3 MB (21.77%)

---
+++ LeetCode/Medium/3100-water-bottles-ii/3100-water-bottles-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        ans = 0
+        while numBottles >= numExchange:
+            ans += numExchange
+            numBottles -= numExchange - 1
+            numExchange += 1
+        ans += numBottles
+        return ans
