3cf7da1f70fbc5c89ae1739dfc0d2a0712bd27a1
Taeyun Eom <tyv001008@gmail.com>
2026-06-13 22:52:00 +0900

    [Medium] Problem: 0386-lexicographical-numbers, Time: 2 ms (100%), Space: 51.2 MB (50%)

--- LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.kt
+++ LeetCode/Medium/0386-lexicographical-numbers/0386-lexicographical-numbers.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun lexicalOrder(n: Int): List<Int> {
         val ans = mutableListOf<Int>()
-        fun dfs(p: String) {
-            ans += p.toInt()
-            for (i in 0..9) if ((p + i.toString()).toInt() <= n) dfs(p + i.toString())
+        fun dfs(p: Int) {
+            ans += p
+            for (i in 0..9) if (p * 10 + i <= n) dfs(p * 10 + i)
         }
-        for (i in 1..9) if (i <= n) dfs(i.toString())
+        for (i in 1..9) if (i <= n) dfs(i)
         return ans
     }
 }
