830480feae954f66c974bdfb5fc85e1a1cf445b3
Taeyun Eom <tyv001008@gmail.com>
2026-06-13 22:08:30 +0900

    [Medium] Problem: 0139-word-break, Time: 28 ms (7.04%), Space: 46.9 MB (7.04%)

--- LeetCode/Medium/0139-word-break/0139-word-break.kt
+++ LeetCode/Medium/0139-word-break/0139-word-break.kt
@@ -1,35 +1,9 @@
-class Trie() {
-    class Node(var isEnd: Boolean) {
-        val map: MutableMap<Char, Node> = mutableMapOf()
-    }
-
-    val root = Node(false)
-
-    fun insert(word: String) {
-        var curr = root
-        for (i in word.indices) {
-            curr = curr.map.getOrPut(word[i]) { Node(false) }
-            if (i == word.length - 1) curr.isEnd = true
-        }
-    }
-
-    fun search(word: String): Boolean {
-        var curr = root
-        for (i in word.indices) {
-            curr = curr.map.getOrElse(word[i]) { return false }
-            if (i == word.length - 1) return curr.isEnd
-        }
-        return false
-    }
-}
-
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
-        val trie = Trie()
-        for (w in wordDict) trie.insert(w)
+        val dict = wordDict.toSet()
         val dp = BooleanArray(s.length)
         for (i in s.indices) if (i == 0 || dp[i-1]) {
-            for (j in i..s.length) if (trie.search(s.substring(i, j))) dp[j - 1] = true
+            for (j in i..s.length) if (dict.contains(s.substring(i, j))) dp[j - 1] = true
         }
         return dp.last()
     }
